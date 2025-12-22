6874b112983bfd8f39c6678ac0d9834b05f40870
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-12-22 16:32:49 +0900

    Initial Project

---
+++ src/main/kotlin/com/nekozouneko/antiCivBreak/AntiCivBreak.kt
@@ -0,0 +1,14 @@
+package com.nekozouneko.antiCivBreak
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AntiCivBreak : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
